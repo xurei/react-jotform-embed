f66488747e69800e41a128672fd57defb4ea28bf
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-11-21 20:11:20 +0100

    Removed story

--- stories/index.js
+++ stories/index.js
@@ -6,5 +6,4 @@ const JobformEmbed = require('../src/react-jotform-embed');
 
 storiesOf('JobformEmbed', module)
 .add('with text', () => <JobformEmbed src="https://form.jotformeu.com/73244192484358" />)
-.add('with long text', () => <JobformEmbed src="https://form.jotformeu.com/72293297018360" />)
 ;
