9d0bd8eb4be84ab6c37fc7251f2c4169641fb8ae
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-11-21 22:17:09 +0100

    Fixed stories

--- stories/index.js
+++ stories/index.js
@@ -2,8 +2,7 @@ import React from 'react'; //eslint-disable-line no-unused-vars
 
 import { storiesOf } from '@storybook/react';
 
-const JobformEmbed = require('../src/react-jotform-embed');
+import JotformEmbed from '../src/react-jotform-embed';
 
 storiesOf('JobformEmbed', module)
-.add('with text', () => <JobformEmbed src="https://form.jotformeu.com/73244192484358" />)
-;
+.add('with text', () => <JotformEmbed src="https://form.jotformeu.com/73244192484358" />);
